46beaeeb1341b8047325b02828a4bc96285adac5
Pragun Sharma <psharma5@ucsc.edu>
2017-06-27 13:08:35 -0700

    Initial Commit

---
+++ FlappyBirdGameTests/FlappyBirdGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlappyBirdGameTests.swift
+//  FlappyBirdGameTests
+//
+//  Created by Pragun Sharma on 27/06/17.
+//  Copyright © 2017 Pragun Sharma. All rights reserved.
+//
+
+import XCTest
+@testable import FlappyBirdGame
+
+class FlappyBirdGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
